7c4da57be2d0df05a04f03769728488bb1131b95
next <364574103@qq.com>
2025-10-31 17:23:56 +0800

    PPO_fixed

--- PPO/Train.py
+++ PPO/Train.py
@@ -1,6 +1,13 @@
 """
 PPO训练主程序
 """
+
+# V1.0  2025.10.28      --- by next, 初步实现了使用Renet深度学习模型架构的PPO强化学习模型的搭建与训练等
+# V1.1  2025.10.31      --- by next, 针对于V1.0的训练进行了整理和归类，便于以后的扩展等
+
+
+
+
 import torch
 import os
 import logging
